7ddf95db6f794983362dbb1bd532acedcb60a2eb
Marco Berardini <mberardini@evolution-man.com>
2016-09-03 13:39:51 -0700

    [client] Created mutiple routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,12 +2,18 @@ import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 import {AlbumsComponent}  from './albums.component';
+import {AlbumComponent}  from './album.component'
 import {ContactComponent} from './contact.component';
+import {HomeComponent} from './home.component';
+import {ArchivesComponent} from './archives.component';
 
 
 
 @RouteConfig([
     {path:'/albums' , name:'Albums' , component: AlbumsComponent  , useAsDefault: true},
+    {path:'/albums/:id', name:'Album', component: AlbumComponent},
+    {path:'/home', name:'Home', component: HomeComponent},
+    {path:'/archives/:year/:month', name:'Archives', component: ArchivesComponent},
     {path:'/contact', name:'Contact', component: ContactComponent},
     {path:'/*other' , name: 'Other' , redirectTo: ['Albums']}
 ])
--- app/contact.component.ts
+++ app/contact.component.ts
@@ -1,10 +1,24 @@
 import {Component} from 'angular2/core';
+import {Router, CanDeactivate} from 'angular2/router';
+
 
 @Component({
     templateUrl: '/app/contact.component.html'
 })
-export class ContactComponent {
+export class ContactComponent implements CanDeactivate {
+    constructor(private _router: Router){
+
+    }
     onSubmit(form){
         console.log(form);
+        this._router.navigate(['Albums']);
+    }
+
+    routerCanDeactivate(next, previous){
+        console.log("next ++++++++", next);
+        console.log("prevous ++++++++", previous);
+        // if(this.form.dirty)
+        // you would need to have a model driven form to check if it is dirty
+        return confirm("Are you sure");
     }
 }
